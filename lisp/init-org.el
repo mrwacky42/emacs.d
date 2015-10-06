3df84ac639ebf249b8c5aee0504ba3bc60ad3dd3
Sharif Nassar <sharif@mrwacky.com>
2015-10-05 20:30:42 -0700

    Agenda files.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -2,9 +2,10 @@
   :defer
   :config
   (use-package org-habit)
-  (setq org-agenda-start-on-weekday nil
-        org-directory "~/info/orgfiles"
-        org-default-notes-file (concat org-directory "/notes.org"))
+  (setq org-agenda-files (list "~/info/orgfiles") ; default "~/.agenda_files"
+        org-agenda-start-on-weekday nil
+        org-default-notes-file (concat org-directory "/notes.org")
+        org-directory "~/info/orgfiles")
   (add-to-list 'org-modules 'org-habit)
   :bind
   ("C-c a" . org-agenda)
