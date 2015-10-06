51673110eb5a12369075ad8d2dda01253c03e64b
Sharif Nassar <sharif@mrwacky.com>
2015-10-05 20:30:50 -0700

    Customisations

--- custom.el
+++ custom.el
@@ -3,14 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cperl-close-paren-offset -4)
+ '(cperl-continued-statement-offset 4)
+ '(cperl-indent-level 4)
+ '(cperl-indent-parens-as-block t)
  '(magit-diff-options (quote ("--ignore-space-change" "--ignore-all-space")))
  '(paradox-github-token t)
  '(safe-local-variable-values
    (quote
     ((c-default-style . "linux")
      (whitespace-line-column . 80)
-     (lexical-binding . t))))
- )
+     (lexical-binding . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
