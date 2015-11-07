5859ca29b3405db16cb1e28889ff106a38d35d83
Sharif Nassar <sharif@mrwacky.com>
2015-11-06 19:31:00 -0800

    noise.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -12,5 +12,9 @@
   ("C-c l" . org-store-link)
   ("C-c c" . org-capture))
 
+;; Attempts at prettier timestamps
+;; (setq org-time-stamp-custom-formats '("<%Y-%m-%d>" . "<%Y-%m-%d %H:%M>"))
+;; (setq org-display-custom-times t)
+
 (provide 'init-org)
 ;;; init-org.el ends here
