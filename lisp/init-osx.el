2c846ef09dc427dc7c1f5f3ef8c20ad1749675fa
Sharif Nassar <sharif@mrwacky.com>
2015-12-19 17:31:52 -0800

    Remove (when) wrapper
    init-osx is already conditionally required from init.el.

--- lisp/init-osx.el
+++ lisp/init-osx.el
@@ -4,21 +4,22 @@
 
 ;;; Code:
 
-(when *is-a-mac*
 
-  ;; Mouse scrolling goes crazy in OSX
-  ;; See also:
-  ;; http://stackoverflow.com/a/25438277
-  ;; https://github.com/purcell/emacs.d/commit/99128a7c801e3c938b9d029275b607bb12d0a2ea#diff-2fc9ca58848b1b2a948d59fb006501fa
-  (mouse-wheel-mode -1)
-  (dolist (multiple '("" "double-" "triple-"))
-    (dolist (direction '("right" "left" "up" "down"))
-      (global-set-key (kbd (concat "<" multiple "wheel-" direction ">")) 'ignore)))
+;; Mouse scrolling goes crazy in OSX
+;; See also:
+;; http://stackoverflow.com/a/25438277
+;; https://github.com/purcell/emacs.d/commit/99128a7c801e3c938b9d029275b607bb12d0a2ea#diff-2fc9ca58848b1b2a948d59fb006501fa
+(mouse-wheel-mode -1)
+(dolist (multiple '("" "double-" "triple-"))
+  (dolist (direction '("right" "left" "up" "down"))
+    (global-set-key (kbd (concat "<" multiple "wheel-" direction ">")) 'ignore)))
 
-  ;; From: http://emacsredux.com/blog/2015/05/09/emacs-on-os-x/
-  ;; Although he does not say what the problem is.
-  ;; I am not a dired power user.
-  (setq insert-directory-program (executable-find "gls")))
+;; From: http://emacsredux.com/blog/2015/05/09/emacs-on-os-x/
+;; Although he does not say what the problem is.
+;; I am not a dired power user.
+(setq insert-directory-program (executable-find "gls"))
+
+(add-to-list 'ido-ignore-files "\\.DS_Store")
 
 (provide 'init-osx)
 ;;; init-osx ends here
