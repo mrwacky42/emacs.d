efe60ea75e1bac638fb15a475a25aceab2acdc8f
Sharif Nassar <sharif@mrwacky.com>
2016-01-11 14:22:55 -0800

    magit-revert-buffers is sloooooow

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -9,10 +9,11 @@
              :bind ("C-c g" . magit-status)
              :config
              (fullframe magit-status magit-mode-quit-window)
-             (setq magit-save-some-buffers nil
-                   magit-process-popup-time 10
+             (setq magit-completing-read-function 'magit-ido-completing-read
                    magit-diff-refine-hunk t
-                   magit-completing-read-function 'magit-ido-completing-read)
+                   magit-process-popup-time 10
+                   magit-revert-buffers nil
+                   magit-save-some-buffers nil)
              ;; Hint: customize `magit-repo-dirs' so that you can use C-u M-F12 to
              ;; quickly open magit on any one of your projects.
              (global-set-key [(meta f12)] 'magit-status)
