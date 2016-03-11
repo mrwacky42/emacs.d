7d6e6b320614d07544c93e9d9d3f849bcc1937af
Sharif Nassar <sharif@mrwacky.com>
2016-03-11 09:15:20 -0800

    Fix paredit to actually enable

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -5,13 +5,13 @@
 
 (use-package paredit
   :ensure
-  :commands paredit-mode
   :diminish paredit-mode
   :config
+  (autoload 'enable-paredit-mode "paredit" "Turn on pseud-structural editing of Lisp code.")
   (define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp)
   (define-key paredit-mode-map (kbd "M-(") 'paredit-backward-slurp-sexp)
   (dolist (mode paren-face-modes)
-    (add-hook (intern (concat (symbol-name mode) "-hook")) 'paredit-mode)))
+    (add-hook (intern (concat (symbol-name mode) "-hook")) #'enable-paredit-mode)))
 
 (use-package elisp-slime-nav
   :ensure
