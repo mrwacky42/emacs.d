9914816b1afeb4124f47aab113e55f56b5c97c06
Sharif Nassar <mrwacky42@users.noreply.github.com>
2017-01-25 22:09:58 -0800

    Cleanup init-perl, removing lots of cruft

--- lisp/init-perl.el
+++ lisp/init-perl.el
@@ -1,21 +1,16 @@
 
-(require 'cperl-mode)
-(defalias 'perl-mode 'cperl-mode)      ; always use cperl-mode
-
-(custom-set-variables
- '(cperl-close-paren-offset -4)
- '(cperl-continued-statement-offset 4)
- '(cperl-indent-level 4)
- '(cperl-indent-parens-as-block t))
-
-(define-key cperl-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
-(define-key cperl-mode-map (kbd "C-M-h") 'backward-kill-word)
-(global-set-key (kbd "C-h P") 'cperl-perldoc)
-
-(add-to-list 'auto-mode-alist '("\\.p[lm]$" . cperl-mode))
-(add-to-list 'auto-mode-alist '("\\.pod$" . pod-mode))
-(add-to-list 'auto-mode-alist '("\\.tt$" . tt-mode))
-(add-to-list 'auto-mode-alist '("\\.t\\'" . cperl-mode))
+(use-package cperl-mode
+  :mode ("\\.t\\'" . cperl-mode)
+  :init (defalias 'perl-mode 'cperl-mode) ;; always use cperl-mode
+  :config
+  (custom-set-variables
+   '(cperl-close-paren-offset -4)
+   '(cperl-continued-statement-offset 4)
+   '(cperl-indent-level 4)
+   '(cperl-indent-parens-as-block t))
+  ;; http://stackoverflow.com/questions/318553/getting-emacs-to-untabify-when-saving-certain-file-types-and-only-those-file-typ
+  (add-hook 'cperl-mode-hook
+            '(lambda () (add-hook 'before-save-hook 'perltidy-buffer nil t))))
 
 ;; Consider also factoring out the executable-find bit:
 ;; http://emacs-fu.blogspot.com/2008/12/running-some-external-program-only-if.html
@@ -33,14 +28,13 @@
 
 (defun perltidy-buffer () "Run perltidy on current buffer."
        (interactive)
-       (let ((where_i_was (point)))
-         (save-excursion (mark-whole-buffer)
-                         (perltidy-region))
-         (goto-char where_i_was)))
+       (if (executable-find "perltidy")
+           (let ((where_i_was (point)))
+             (shell-command-on-region (point-min) (point-max) "perltidy -q" nil t)
+             (goto-char where_i_was))
+         (message "Unable to find perltidy")))
 
-;;http://stackoverflow.com/questions/318553/getting-emacs-to-untabify-when-saving-certain-file-types-and-only-those-file-typ
-(add-hook 'cperl-mode-hook
-          '(lambda ()
-             (add-hook 'before-save-hook 'perltidy-buffer nil t)))
+
+
 
 (provide 'init-perl)
