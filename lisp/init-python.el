0fe785a75b379aa549c6f3bbc1fa291ffd169aa1
Sharif Nassar <mrwacky42@users.noreply.github.com>
2017-12-18 14:27:45 -0800

    Python enhance

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -11,10 +11,16 @@
   (delete 'elpy-module-highlight-indentation elpy-modules)
   (delete 'elpy-module-flymake elpy-modules)
 
-  (when (executable-find "ipython") (elpy-use-ipython)))
+  (when (executable-find "ipython")
+    (elpy-use-ipython)
+    (setq python-shell-interpreter "ipython" python-shell-interpreter-args "--simple-prompt --pprint")))
 
 (use-package jinja2-mode
   :ensure)
 
+(use-package electric-operator
+  :ensure
+  :config
+  (add-hook 'python-mode-hook #'electric-operator-mode))
 (provide 'init-python)
 ;;; init-python.el ends here
