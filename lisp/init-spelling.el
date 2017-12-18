6ac3236a4e217a3733201aba96063ab6b566e1e4
Sharif Nassar <mrwacky42@users.noreply.github.com>
2017-12-18 14:30:30 -0800

    Move auto-abbrev magick to new home

--- lisp/init-spelling.el
+++ lisp/init-spelling.el
@@ -19,5 +19,36 @@
     (add-hook 'prog-mode-hook 'flyspell-prog-mode)))
 
 
+
+;; http://endlessparentheses.com/ispell-and-abbrev-the-perfect-auto-correct.html
+(define-key ctl-x-map "\C-i"
+  #'endless/ispell-word-then-abbrev)
+
+(defun endless/ispell-word-then-abbrev (p)
+  "Call `ispell-word', then create an abbrev for it.
+With prefix P, create local abbrev. Otherwise it will
+be global."
+  (interactive "P")
+  (let (bef aft)
+    (save-excursion
+      (while (and (setq bef (thing-at-point 'word))
+                  (not (ispell-word nil 'quiet))
+                  (not (bobp)))
+        (backward-word))
+      (setq aft (thing-at-point 'word)))
+    (if (and aft bef (not (equal aft bef)))
+        (let ((aft (downcase aft))
+              (bef (downcase bef)))
+          (define-abbrev
+            (if p local-abbrev-table global-abbrev-table)
+            bef aft)
+          (message "\"%s\" now expands to \"%s\" %sally"
+                   bef aft (if p "loc" "glob")))
+      (user-error "No typo at or before point"))))
+
+(setq save-abbrevs 'silently
+      abbrev-file-name (concat emacs-etc "abbrev_defs"))
+(setq-default abbrev-mode t)
+
 (provide 'init-spelling)
 ;;; init-spelling.el ends here
--- lisp/misc.el
+++ lisp/misc.el
@@ -43,36 +43,6 @@ The optional NEW-WINDOW argument is not used.  It had better be Chrome or Chromi
 ;; (add-hook 'minibuffer-setup-hook #'my-minibuffer-setup-hook)
 ;; (add-hook 'minibuffer-exit-hook #'my-minibuffer-exit-hook)
 
-
-;; http://endlessparentheses.com/ispell-and-abbrev-the-perfect-auto-correct.html
-(define-key ctl-x-map "\C-i"
-  #'endless/ispell-word-then-abbrev)
-
-(defun endless/ispell-word-then-abbrev (p)
-  "Call `ispell-word', then create an abbrev for it.
-With prefix P, create local abbrev. Otherwise it will
-be global."
-  (interactive "P")
-  (let (bef aft)
-    (save-excursion
-      (while (and (setq bef (thing-at-point 'word))
-                  (not (ispell-word nil 'quiet))
-                  (not (bobp)))
-        (backward-word))
-      (setq aft (thing-at-point 'word)))
-    (if (and aft bef (not (equal aft bef)))
-        (let ((aft (downcase aft))
-              (bef (downcase bef)))
-          (define-abbrev
-            (if p local-abbrev-table global-abbrev-table)
-            bef aft)
-          (message "\"%s\" now expands to \"%s\" %sally"
-                   bef aft (if p "loc" "glob")))
-      (user-error "No typo at or before point"))))
-
-(setq save-abbrevs 'silently
-      abbrev-file-name (concat emacs-etc "abbrev_defs"))
-(setq-default abbrev-mode t)
 
 
 ;; http://endlessparentheses.com/improving-emacs-file-name-completion.html
