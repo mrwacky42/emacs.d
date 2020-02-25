143779bf1ddc4c43cf3e9056a78431b1788991bc
Sharif Nassar <mrwacky42@users.noreply.github.com>
2020-02-25 09:55:03 -0800

    LSP and eldoc for golang

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -6,7 +6,12 @@
   :ensure
   :config
   (add-hook 'before-save-hook #'gofmt-before-save)
-  (add-hook 'go-mode-hook 'wacky-tab-hook))
+  (add-hook 'go-mode-hook 'wacky-tab-hook)
+  (add-hook 'go-mode-hook 'lsp-deferred)
+  (use-package go-eldoc
+    :ensure
+    :config
+    (add-hook 'go-mode-hook 'go-eldoc-setup)))
 
 (provide 'init-go)
 ;;; init-go.el ends here
