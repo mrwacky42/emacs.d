99c8b395966545ca867cbb8a378b9d024e642cc8
Sharif Nassar <mrwacky42@users.noreply.github.com>
2022-12-27 15:12:05 -0800

    custom.el updates

--- custom.el
+++ custom.el
@@ -7,18 +7,19 @@
  '(cperl-continued-statement-offset 4)
  '(cperl-indent-level 4)
  '(cperl-indent-parens-as-block t)
- '(magit-diff-options (quote ("--ignore-space-change" "--ignore-all-space")))
+ '(magit-diff-options '("--ignore-space-change" "--ignore-all-space"))
  '(package-selected-packages
-   (quote
-    (ace-jump-mode ansible ansible-doc anzu apache-mode beacon bug-reference-github cargo coffee-mode color-identifiers-mode comment-dwim-2 company-terraform copy-as-format dired+ dockerfile-mode dynamic-fonts editorconfig electric-operator elisp-slime-nav elpy exec-path-from-shell expand-region flycheck flycheck-pony flycheck-rust flymake-coffee flymake-jshint flymake-json flymake-puppet flymake-python-pyflakes forge frame-restore fullframe gh-md gist git-blame git-link git-messenger git-timemachine gitconfig-mode github-browse-file github-clone gitignore-mode go-mode graphviz-dot-mode ibuffer-vc ido-completing-read+ ido-ubiquitous jinja2-mode jq-mode json-mode magit magit-gh-pulls magit-org-todos magit-svn markdown-mode markdown-toc multi-line multiple-cursors nginx-mode org org-bullets org-present ox-gfm ox-reveal paradox paredit paren-face php-mode pony-mode projectile puppet-mode rust-mode scala-mode session sicp smex smooth-scrolling ssh-config-mode string-inflection swiper switch-window terraform-mode tide toml-mode try tss typescript-mode undo-tree unfill use-package web-mode yaml-mode zop-to-char)))
+   '(ace-jump-mode ansible ansible-doc anzu apache-mode async beacon bug-reference-github cargo coffee-mode color-identifiers-mode comment-dwim-2 company company-terraform copy-as-format counsel-jq dired+ dockerfile-mode dynamic-fonts editorconfig electric-operator elisp-slime-nav elpy engine-mode exec-path-from-shell expand-region flycheck flycheck-pony flycheck-rust flymake-coffee flymake-jshint flymake-json flymake-puppet flymake-python-pyflakes forge frame-restore fullframe gh-md gist git-blame git-link git-messenger git-modes git-timemachine gitconfig-mode github-browse-file github-clone gitignore-mode go-eldoc go-mode graphviz-dot-mode ibuffer-vc ido-completing-read+ ido-ubiquitous jinja2-mode jq-mode json-mode lsp-mode magit magit-gh-pulls magit-org-todos magit-svn markdown-mode markdown-toc multi-line multiple-cursors nginx-mode org org-bullets org-plus-contrib org-present ox-gfm ox-reveal paradox paredit paren-face php-mode pony-mode projectile puppet-mode rust-mode scala-mode session sicp smex smooth-scrolling ssh-config-mode string-inflection swiper switch-window terraform-mode tide toml-mode tree-sitter tree-sitter-langs try tss typescript-mode undo-tree unfill use-package web-mode which-key yaml-mode yasnippet yasnippet-snippets zop-to-char))
  '(paradox-github-token t)
  '(safe-local-variable-values
-   (quote
-    ((c-default-style . "linux")
+   '((vc-prepare-patches-separately)
+     (diff-add-log-use-relative-names . t)
+     (vc-git-annotate-switches . "-w")
+     (c-default-style . "linux")
      (indent-tabs-mode . 1)
      (lexical-binding . t)
      (text-mode . 1)
-     (whitespace-line-column . 80)))))
+     (whitespace-line-column . 80))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
